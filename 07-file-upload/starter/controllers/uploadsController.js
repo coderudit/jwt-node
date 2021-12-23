7f8f92cbfb09aa7a9aee1f109a4eb2eb65091551
Udit Gandhi <coder.udit@gmail.com>
2021-12-23 17:13:15 -0400

    Done with file upload

--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -1,8 +1,28 @@
 const { StatusCodes } = require("http-status-codes");
 const path = require("path");
+const CustomError = require("../errors");
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
 
-const uploadProductImage = async (req, res) => {
-  let productImage = req.files.image;
+const uploadProductImageLocal = async (req, res) => {
+  //check if file exists
+  //check format
+  //check JSON
+
+  if (!req.files) {
+    throw new CustomError.BadRequestError("No file uploaded.");
+  }
+
+  const productImage = req.files.image;
+
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new CustomError.BadRequestError("Please upload image.");
+  }
+
+  const maxSize = 1024 * 1024;
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError("Please upload small image.");
+  }
   const imagePath = path.join(
     __dirname,
     `../public/uploads/${productImage.name}`
@@ -11,6 +31,16 @@ const uploadProductImage = async (req, res) => {
   return res
     .status(StatusCodes.OK)
     .json({ image: { src: `/uploads/${productImage.name}` } });
+};
+
+const uploadProductImage = async (req, res) => {
+  const result = await cloudinary.uploader.upload(
+    req.files.image.tempFilePath,
+    { use_filename: true, folder: "file_upload" }
+  );
+  fs.unlinkSync(req.files.image.tempFilePath);
+  console.log(result);
+  return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
 };
 
 module.exports = { uploadProductImage };
